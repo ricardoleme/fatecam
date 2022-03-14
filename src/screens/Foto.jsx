aaad41d0c2a04b9936fc4c580bac19869ce442e5
glauco.todesco <glauco.todesco@gmail.com>
2022-03-14 20:34:48 -0300

    Implementado o menu Drawer

---
+++ src/screens/Foto.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Foto() {
+    return (
+        <View style={{
+            flex: 1,
+            alignItems: 'center', justifyContent: 'center'
+        }}>
+            <Text>Foto</Text>
+        </View>
+    )
+}
+
+export default Foto
